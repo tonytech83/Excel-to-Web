438b29e7295a97529edcba8842909997dc1aa1ce
Anton Petrov <a.petrov.83@gmail.com>
2022-05-29 20:36:20 +0300

    removed file

--- excel_to_web.py
+++ excel_to_web.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import streamlit
 import streamlit as st
 import plotly.express as px
 from PIL import Image
@@ -7,8 +8,8 @@ st.set_page_config(page_title='Survey Results')
 st.header('Survey Results 2021')
 st.subheader('Was the tutorial helpful?')
 
-### --- LOAD DATAFRAME
-excel_file = 'Survey_Results.xlsx'
+# LOAD DATAFRAME
+excel_file = 'C:\\pythonProjects\\Excel-to-Web\\test.xlsx'
 sheet_name = 'DATA'
 
 df = pd.read_excel(excel_file,
@@ -17,7 +18,7 @@ df = pd.read_excel(excel_file,
                    header=3)
 
 df_participants = pd.read_excel(excel_file,
-                                sheet_name= sheet_name,
+                                sheet_name=sheet_name,
                                 usecols='F:G',
                                 header=3)
 df_participants.dropna(inplace=True)
@@ -27,13 +28,13 @@ department = df['Department'].unique().tolist()
 ages = df['Age'].unique().tolist()
 
 age_selection = st.slider('Age:',
-                        min_value= min(ages),
-                        max_value= max(ages),
-                        value=(min(ages),max(ages)))
+                           min_value=min(ages),
+                           max_value=max(ages),
+                           value=(min(ages), max(ages)))
 
 department_selection = st.multiselect('Department:',
-                                    department,
-                                    default=department)
+                                       department,
+                                       default=department)
 
 # --- FILTER DATAFRAME BASED ON SELECTION
 mask = (df['Age'].between(*age_selection)) & (df['Department'].isin(department_selection))
@@ -50,23 +51,23 @@ bar_chart = px.bar(df_grouped,
                    x='Rating',
                    y='Votes',
                    text='Votes',
-                   color_discrete_sequence = ['#F63366']*len(df_grouped),
-                   template= 'plotly_white')
+                   color_discrete_sequence=['#F63366'] * len(df_grouped),
+                   template='plotly_white')
 st.plotly_chart(bar_chart)
 
 # --- DISPLAY IMAGE & DATAFRAME
-col1, col2 = st.beta_columns(2)
-image = Image.open('images/survey.jpg')
+col1, col2 = st.columns(2)
+image = Image.open('C:\\pythonProjects\\Excel-to-Web\\images\\survey.jpg')
 print(image)
 col1.image(image,
-        caption='Designed by slidesgo / Freepik',
-        use_column_width=True)
+           caption='Designed by slidesgo / Freepik',
+           use_column_width=True)
 col2.dataframe(df[mask])
 
 # --- PLOT PIE CHART
 pie_chart = px.pie(df_participants,
-                title='Total No. of Participants',
-                values='Participants',
-                names='Departments')
+                   title='Total No. of Participants',
+                   values='Participants',
+                   names='Departments')
 
 st.plotly_chart(pie_chart)
